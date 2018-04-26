c992df417ea84aba2165fc841e1a40f2ed07642a
moss94549@gmail.com <moss94549@gmail.com>
2018-04-26 06:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-var number = 10
+var number = 10;
 
 function add5()
 {
-  numner +=5
+  numner +=5;
 }
 
 function divideBy3()
 {
-  number /=3
+  number /=3;
 }
 
-console.log(number)
+console.log(number);
 
-add5()
-console.log(number)
-divideBy3()
-console.log(number)
+add5();
+console.log(number);
 
-inc(n)
+divideBy3();
+console.log(number);
+
+function inc(n)
 {
-  n++
-  return n
+  n++;
+  return n;
 }
 
-dec(n)
+function dec(n)
 {
-  n--
-  return n
+  n--;
+  return n;
 }
-
-}
-
-
