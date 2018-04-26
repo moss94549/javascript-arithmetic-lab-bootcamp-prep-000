e7593c53db8a4f00d6f4f63a6dbeda8cfa585494
moss94549@gmail.com <moss94549@gmail.com>
2018-04-26 06:22:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,45 @@
+function add(a, b) 
+{
+}
+
+function subtract(a, b)
+{
+
+}
+
+function multiply(a, b)
+{
+  
+}
+
+function divide(a, b) 
+{
+  
+}
+
+function inc(n) 
+{
+  
+}
+
+function dec(n) 
+{
+
+}
+
+function makeInt(n)
+{
+  // 'parses n as an integer and returns the parsed integer', function() {
+    expect(makeInt(a.toString())).toEqual(a)
+
+  // 'assumes base 10'
+
+  // 'returns NaN as appropriate'
+}
+
+function preserveDecimal(n)
+{
+  // preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number
+
+  // returns NaN as appropriate
+}
